5d408ba0c2e5e500167982b350f52c95f20783a6
seda22 <sedakacarr@gmail.com>
2026-02-13 20:48:08 +0300

    fix: update asset paths to work with Vercel production build

--- FakeDataList.js
+++ FakeDataList.js
@@ -1,37 +1,37 @@
 const FakeDataList = [
   {
     id: "1",
-    imgUrl: "/public/assets/iteration-2/icons/1.svg",
+    imgUrl: "/assets/iteration-2/icons/1.svg",
     alt: "kore",
     text: "YENİ! Kore",
   },
   {
     id: "2",
-    imgUrl: "/public/assets/iteration-2/icons/2.svg",
+    imgUrl: "/assets/iteration-2/icons/2.svg",
     alt: "pizza",
     text: "Pizza",
   },
   {
     id: "3",
-    imgUrl: "/public/assets/iteration-2/icons/3.svg",
+    imgUrl: "/assets/iteration-2/icons/3.svg",
     alt: "burger",
     text: "Burger",
   },
   {
     id: "4",
-    imgUrl: "/public/assets/iteration-2/icons/4.svg",
+    imgUrl: "/assets/iteration-2/icons/4.svg",
     alt: "kızartmalar",
     text: "Kızartmalar",
   },
   {
     id: "5",
-    imgUrl: "/public/assets/iteration-2/icons/5.svg",
+    imgUrl: "/assets/iteration-2/icons/5.svg",
     alt: "fast food",
     text: "Fast food",
   },
   {
     id: "6",
-    imgUrl: "/public/assets/iteration-2/icons/6.svg",
+    imgUrl: "/assets/iteration-2/icons/6.svg",
     alt: "gazlı icecek",
     text: "Gazlı İçecek",
   },
--- FakeDataMenuList.js
+++ FakeDataMenuList.js
@@ -1,42 +1,42 @@
 const FakeDataMenuList = [
   {
     id: "1",
-    imgUrl: "./public/assets/iteration-2/icons/1.svg",
+    imgUrl: "/assets/iteration-2/icons/1.svg",
     alt: "Ramen",
     text: "Ramen",
     active: false,
   },
   {
     id: "2",
-    imgUrl: "./public/assets/iteration-2/icons/2.svg",
+    imgUrl: "/assets/iteration-2/icons/2.svg",
     alt: "Pizza",
     text: "Pizza",
     active: true,   // burada active vardı
   },
   {
     id: "3",
-    imgUrl: "./public/assets/iteration-2/icons/3.svg",
+    imgUrl: "/assets/iteration-2/icons/3.svg",
     alt: "Burger",
     text: "Burger",
     active: false,
   },
   {
     id: "4",
-    imgUrl: "./public/assets/iteration-2/icons/4.svg",
+    imgUrl: "/assets/iteration-2/icons/4.svg",
     alt: "French fries",
     text: "French fries",
     active: false,
   },
   {
     id: "5",
-    imgUrl: "./public/assets/iteration-2/icons/5.svg",
+    imgUrl: "/assets/iteration-2/icons/5.svg",
     alt: "Fast food",
     text: "Fast food",
     active: false,
   },
   {
     id: "6",
-    imgUrl: "./public/assets/iteration-2/icons/6.svg",
+    imgUrl: "/assets/iteration-2/icons/6.svg",
     alt: "Soft drinks",
     text: "Soft drinks",
     active: false,
